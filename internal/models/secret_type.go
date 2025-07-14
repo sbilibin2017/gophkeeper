329c28036d65402ad1547f140cbc6bcb8ae994cb
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-07-14 06:47:08 +0300

    Добавил константы для типов секрета

---
+++ internal/models/secret_type.go
@@ -0,0 +1,8 @@
+package models
+
+const (
+	SecretTypeBankCard         = "bank-card"
+	SecretTypeBinary           = "binary"
+	SecretTypeUsernamePassword = "username-password"
+	SecretTypeText             = "text"
+)
