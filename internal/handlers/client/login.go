1e357c4f276be01bc0227a575347f6b990fc6dc7
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-07-20 07:57:35 +0300

    Added client auth handlers

---
+++ internal/handlers/client/login.go
@@ -0,0 +1,84 @@
+package client
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/sbilibin2017/gophkeeper/internal/configs"
+	"github.com/sbilibin2017/gophkeeper/internal/facades/auth"
+	"github.com/sbilibin2017/gophkeeper/internal/models"
+	"github.com/sbilibin2017/gophkeeper/internal/repositories/bankcard"
+	"github.com/sbilibin2017/gophkeeper/internal/repositories/binary"
+	"github.com/sbilibin2017/gophkeeper/internal/repositories/text"
+	"github.com/sbilibin2017/gophkeeper/internal/repositories/user"
+	pb "github.com/sbilibin2017/gophkeeper/pkg/grpc/auth"
+)
+
+func LoginHTTP(
+	ctx context.Context,
+	username, password, authURL, tlsCertFile, tlsKeyFile string,
+) (*models.AuthResponse, error) {
+	config, err := configs.NewClientConfig(
+		configs.WithHTTPClient(authURL, tlsCertFile, tlsKeyFile, ""),
+	)
+	if err != nil {
+		return nil, err
+	}
+	if config.HTTPClient == nil {
+		return nil, fmt.Errorf("HTTP client is not configured for URL: %s", authURL)
+	}
+
+	bankcard.CreateClientTable(ctx, config.DB)
+	text.CreateClientTable(ctx, config.DB)
+	binary.CreateClientTable(ctx, config.DB)
+	user.CreateClientTable(ctx, config.DB)
+
+	authReq := &models.AuthRequest{
+		Username: username,
+		Password: password,
+	}
+
+	facade := auth.NewLoginHTTPFacade(config.HTTPClient)
+
+	resp, err := facade.Login(ctx, authReq)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func LoginGRPC(
+	ctx context.Context,
+	username, password, authURL, tlsCertFile, tlsKeyFile string,
+) (*models.AuthResponse, error) {
+	config, err := configs.NewClientConfig(
+		configs.WithGRPCClient(authURL, tlsCertFile, tlsKeyFile, ""),
+	)
+	if err != nil {
+		return nil, err
+	}
+	if config.GRPCClient == nil {
+		return nil, fmt.Errorf("gRPC client is not configured for URL: %s", authURL)
+	}
+
+	bankcard.CreateClientTable(ctx, config.DB)
+	text.CreateClientTable(ctx, config.DB)
+	binary.CreateClientTable(ctx, config.DB)
+	user.CreateClientTable(ctx, config.DB)
+
+	authReq := &models.AuthRequest{
+		Username: username,
+		Password: password,
+	}
+
+	grpcClient := pb.NewAuthServiceClient(config.GRPCClient)
+	facade := auth.NewLoginGRPCFacade(grpcClient)
+
+	resp, err := facade.Login(ctx, authReq)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
