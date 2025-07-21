6dd0c3f7cfad561d9764245da7d1af74488304fb
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-07-21 04:17:09 +0300

    Refactored models

---
+++ internal/models/secret_type.go
@@ -0,0 +1,9 @@
+package models
+
+// SecretType constants define the types of secrets supported in the system.
+const (
+	SecretTypeBankCard = "bank_card"
+	SecretTypeBinary   = "binary"
+	SecretTypeText     = "text"
+	SecretTypeUser     = "user"
+)
