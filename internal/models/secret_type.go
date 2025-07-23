82320a0be40c2a0d27889789359cd5acd35f1a82
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-07-23 06:17:45 +0300

    Added secret type constants

---
+++ internal/models/secret_type.go
@@ -0,0 +1,8 @@
+package models
+
+const (
+	SecretTypeBankCard = "bankcard" // Secret type: BankCard
+	SecretTypeUser     = "user"     // Secret type: User
+	SecretTypeText     = "text"     // Secret type: Text
+	SecretTypeBinary   = "binary"   // Secret type: Binary
+)
