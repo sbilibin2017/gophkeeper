1849df3c977a73446cf8ff38cb06f3e914e654bc
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-07-23 06:33:24 +0300

    Added aes key enc to EncryptedSecret struct

--- internal/models/encryped_secret.go
+++ internal/models/encryped_secret.go
@@ -6,6 +6,7 @@ type EncrypedSecret struct {
 	SecretType string `json:"secret_type"`         // Type of the secret (e.g., bankcard, user, text)
 	Ciphertext []byte `json:"ciphertext"`          // Encrypted secret data (output of asymmetric encryption)
 	HMAC       []byte `json:"hmac"`                // HMAC for verifying integrity/authenticity
+	AESKeyEnc  []byte `json:"aes_key_enc"`         // RSA-encrypted AES key
 	Nonce      []byte `json:"nonce,omitempty"`     // Optional nonce/IV for hybrid encryption (e.g., AES-GCM)
 	KeyID      string `json:"key_id,omitempty"`    // Optional identifier of public key used for encryption
 	Timestamp  int64  `json:"timestamp,omitempty"` // Optional Unix timestamp when encryption occurred
