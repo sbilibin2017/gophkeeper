417bc84bda529c7f37fdb89b32ae3638baa967d9
Sergey Bilibin <s.bilibin2017@yandex.ru>
2025-08-16 08:09:59 +0300

    Added models

--- internal/models/secret_key.go
+++ internal/models/secret_key.go
@@ -7,7 +7,7 @@ type SecretKeyDB struct {
 	SecretKeyID     string    `json:"secret_key_id" db:"secret_key_id"`         // уникальный идентификатор записи
 	SecretID        string    `json:"secret_id" db:"secret_id"`                 // идентификатор секрета
 	DeviceID        string    `json:"device_id" db:"device_id"`                 // идентификатор устройства
-	EncryptedAESKey []byte    `json:"encrypted_aes_key" db:"encrypted_aes_key"` // AES ключ, зашифрованный публичным ключом устройства
+	EncryptedAESKey string    `json:"encrypted_aes_key" db:"encrypted_aes_key"` // AES ключ, зашифрованный публичным ключом устройства
 	CreatedAt       time.Time `json:"created_at" db:"created_at"`               // дата создания записи
 	UpdatedAt       time.Time `json:"updated_at" db:"updated_at"`               // дата последнего обновления записи
 }
--- internal/models/user.go
+++ internal/models/user.go
@@ -2,6 +2,31 @@ package models
 
 import "time"
 
+// RegisterRequest представляет тело запроса на регистрацию пользователя.
+type RegisterRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+// LoginRequest представляет тело запроса на аутентификацию пользователя.
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+// RegisterResponse представляет тело ответа сервера на регистрацию пользователя.
+type RegisterResponse struct {
+	UserID     string `json:"user_id"`
+	DeviceID   string `json:"device_id"`
+	PrivateKey string `json:"private_key"`
+	Token      string `json:"token"` // можно оставить пустым, если токен извлекается через TokenGetter
+}
+
+// LoginResponse представляет тело ответа сервера на вход пользователя.
+type LoginResponse struct {
+	Token string `json:"token"` // токен можно хранить здесь, либо извлекать через TokenGetter
+}
+
 // UserDB представляет запись пользователя
 type UserDB struct {
 	UserID       string    `json:"user_id" db:"user_id"`             // уникальный идентификатор пользователя
